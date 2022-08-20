20d74114834f764785d866fa3aa6b93fdf299f48
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-08-20 16:38:20 +0100

    links to sites

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import Name from "./Name";
 import About from "./About";
 import ProjectRight from "./ProjectRight";
 import ProjectLeft from "./ProjectLeft";
-// import Data from "./Data";
-// import ProjectTemplate from "./ProjectTemplate";
+import Betaminds from "./assets/betaminds.png";
+import Groupsoup from "./assets/groupsoup_screenshot.png";
+import Syxxshoots from "./assets/syxxshoots_screenshot.png";
 
 function App() {
   return (
     <>
       <Name />
       <About />
-      <ProjectRight />
-      <ProjectLeft />
-      {/* <Data /> */}
+      <ProjectRight
+        name="Betaminds"
+        tech="React"
+        image={Betaminds}
+        link="https://laughing-pike-87c295.netlify.app/"
+      />
+      <ProjectLeft
+        name="Group Soup"
+        tech="React"
+        image={Groupsoup}
+        link="https://amazing-cajeta-273e87.netlify.app/"
+      />
+      <ProjectRight
+        name="Syxxshoots"
+        tech="Javascript"
+        image={Syxxshoots}
+        link="https://tangerine-halva-e410de.netlify.app/"
+      />
     </>
   );
 }
--- src/ProjectRight.js
+++ src/ProjectRight.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Betaminds from "./assets/betaminds.png";
+// import Betaminds from "./assets/betaminds.png";
 // import Data from "./Data";
 
 export default function ProjectRight(props) {
@@ -12,10 +12,21 @@ export default function ProjectRight(props) {
         <section className="project-info">
           <div className="project-text">
             <h1>
-              Project Name: <strong>Betaminds</strong>
+              Project Name: <strong>{props.name}</strong>
             </h1>
             <h4>Built with: </h4>
-            <h4>HTML, CSS, React</h4>
+            <h4>{props.tech}</h4>
+            <br />
+            <h4>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
+              pulvinar sem, id consectetur augue. Sed tempus, dolor eu aliquet
+              faucibus, est magna consequat leo, at interdum mi sapien quis
+              risus. Quisque ac commodo arcu, vel efficitur sem. Integer semper
+              elementum commodo. Nulla hendrerit erat erat, quis fringilla dolor
+              eleifend a. Aliquam leo orci, fermentum id nisi sed, iaculis
+              iaculis augue. Praesent finibus, dui a accumsan pharetra, dui
+              tortor mattis tortor, at egestas nulla dolor eget libero.
+            </h4>
 
             {/* data from mapped objected here */}
           </div>
@@ -23,9 +34,11 @@ export default function ProjectRight(props) {
         <section className="project-view">
           <div className="project-card">
             <div className="project-image-frame">
-              <img src={Betaminds} alt="Betaminds" />
+              <img src={props.image} alt="project" />
             </div>
-            <a>view project</a>
+            <a href={props.link} target="_blank" rel="noreferrer">
+              view project
+            </a>
           </div>
         </section>
       </div>
